7267b4503ef0e77e89d55ab9f40fd2d8dc7283d9
nandainacio <fp_inacio@hotmail.com>
2021-04-15 19:56:45 -0300

    Inclusão botão check e suas funcionalidades

--- script.js
+++ script.js
@@ -6,22 +6,38 @@ function adicionar(){
 
     item.classList.remove('hidden');
 
-    var icon = document.createElement('i');
-    icon.classList.add('bi');
-    icon.classList.add('bi-x-square-fill');
+    var btnCampo = document.createElement('div');
+    btnCampo.classList.add('btnCampo');
+
+    var iconCheck = document.createElement('i');
+    iconCheck.classList.add('bi');
+    iconCheck.classList.add('bi-check-square-fill');
+    iconCheck.classList.add('check');
+
+    var iconDelete = document.createElement('i');
+    iconDelete.classList.add('bi');
+    iconDelete.classList.add('bi-x-square-fill');
+    iconDelete.classList.add('delete');
+
 
     var nomeTarefa = document.createElement('li');
     nomeTarefa.classList.add('nomeTarefa')
     
     nomeTarefa.innerText = tarefa.value;
     listaTarefas.appendChild(nomeTarefa);
-    nomeTarefa.appendChild(icon);
+    nomeTarefa.appendChild(btnCampo);
+    btnCampo.appendChild(iconCheck);
+    btnCampo.appendChild(iconDelete);
+
 
     tarefa.value = null;    
-    // novoNome.addEventListener("click", function(){
-    //     novoNome.style.textDecoration = "line-through";
-    // })
-    icon.addEventListener("click", function(){
+
+
+    iconCheck.addEventListener("click", function(){
+      nomeTarefa.style.textDecoration = "line-through";
+      nomeTarefa.style.color = "#696969"
+  })
+    iconDelete.addEventListener("click", function(){
       listaTarefas.removeChild(nomeTarefa);
   })
 }
